5d4cd18f06b55169abaedab39da737607ca6094c
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-02-20 11:44:04 +0900

    get postについて

--- boardapp/views.py
+++ boardapp/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # urls.pyからもらったrequesetオブジェクト
 def signupfunc(request):
+	if request.method == "POST":
+		print("ぽすおだよー")	
+	else:
+		print("ちがうねー")
 	return render(request, 'signup.html', { 'some':100 })
