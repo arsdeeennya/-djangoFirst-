a327803312ef42639346b040d3842cb1d0f314af
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-02-28 15:40:55 +0900

    CORS導入

--- boardproject/settings.py
+++ boardproject/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
 	'api.apps.ApiConfig',
 	'rest_framework',
 	'rest_framework.authtoken',
+	'corsheaders',
 ]
 
 MIDDLEWARE = [
+	'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+	"http://localhost:3000"
 ]
 
 ROOT_URLCONF = 'boardproject.urls'
